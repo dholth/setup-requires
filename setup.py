debdf010d5693671b08a01466b3d7b7b73133db8
Daniel Holth <dholth@fastmail.fm>
2015-02-25 09:58:09 -0500

    include package specific setup.py contents inline

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # Install dependencies from a "[metadata] setup-requires = ..." section in
-# setup.cfg, then run real-setup.py.
+# setup.cfg, then run real-setup.py (or inline setup.py)
 # From https://bitbucket.org/dholth/setup-requires
 
 import sys, os, subprocess, codecs, pkg_resources
@@ -33,8 +33,22 @@ try:
 except (configparser.NoSectionError, configparser.NoOptionError):
     pass
 
-# Run real-setup.py
-exec(compile(open("real-setup.py").read().replace('\\r\\n', '\\n'),
-    __file__,
-    'exec'))
+# Run real-setup.py from a separate file
+# exec(compile(open("real-setup.py").read().replace('\\r\\n', '\\n'),
+#     __file__,
+#     'exec'))
+
+### Or place normal setup.py contents below ###
+
+from setuptools import setup
+
+setup(name="example-package",
+    version = "0.0.1",
+    packages = [ 'example' ],
+    install_requires = [ ],
+    description = "An example of a package with setup requirements.",
+    license = "MIT",
+    author = "Emilio Example",
+    author_email = "emilio@example.org",
+    url="https://bitbucket.org/dholth/setup-requires")
 
